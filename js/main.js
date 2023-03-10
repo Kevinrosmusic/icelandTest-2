2cb09e689e72d8f418710d0344369729cb2e1bd8
kevinrosmusic <kevinpalominosanchez1992@gmail.com>
2023-03-10 18:44:01 -0400

    Create nav menu view

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// const section_1 = document.getElementById('section-1');
+// const section_2 = document.getElementById('section-2');
+// const btn_label = document.getElementById('btn-burger');
+// const btn_nav = document.getElementById('btn-nav');
+
+// btn_label.addEventListener("click", () => {
+//   console.log(btn_nav.checked)
+//   if (btn_nav.checked = true) {
+//     section_1.classList.remove('d-block');
+//     section_1.classList.add('d-none');
+//     section_2.classList.remove('d-none');
+//     section_2.classList.add('d-block');
+//   } else if (btn_nav.checked = false) {
+//     section_1.classList.remove('d-none');
+//     section_1.classList.add('d-block');
+//     section_2.classList.remove('d-block');
+//     section_2.classList.add('d-none');
+//   }
+// });
