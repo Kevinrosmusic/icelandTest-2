b61b3e3bc82a76d4f0f4ff0235af156d26e7334b
kevinrosmusic <kevinpalominosanchez1992@gmail.com>
2023-03-11 16:27:35 -0400

    Relocate js variables, make inner comments

--- js/main.js
+++ js/main.js
@@ -1,60 +1,15 @@
-// variables - elements
-const section_1 = document.getElementById('section-1');
-const section_2 = document.getElementById('section-2');
-const btn_label = document.getElementById('btn-burger');
-const btn_nav = document.getElementById('btn-nav');
-const img_container = document.getElementById('img-container');
-const icon_container = document.getElementById('iconContainer');
-const text_container = document.getElementById('text-container');
-const menu_container = document.getElementById('menu-container');
-const menu_link = document.getElementsByClassName('menu-link');
+//IMPORTED VARIABLES
+import { section_1, section_2, btn_label, img_container, icon_container, 
+  text_container, menu_container, menu_link, logo, title, 
+} from  './variables.js';
+
 let boolean = false;
 
-// functions
-btn_label.addEventListener('click', () => {
-  boolean = !boolean;
-   if (boolean) {
-      section_1.classList.remove('d-block');
-      section_1.classList.add('d-none');
-      section_2.classList.remove('d-none');
-      section_2.classList.add('d-block');
+// FUNCTIONS
 
-      img_container.classList.add('animate__fadeInBottomLeft', 'animate__fast');
-      img_container.classList.remove('animate__fadeOutBottomLeft', 'animate__faster');
-
-      icon_container.classList.add('animate__fadeInRight', 'animate__fast');
-      icon_container.classList.remove('animate__fadeOutRight', 'animate__faster');
-
-      text_container.classList.add('animate__backInUp', 'animate__fast');
-      text_container.classList.remove('animate__backOutDown', 'animate__faster');
-
-      menu_container.classList.add('animate__fadeInUp', 'animate__fast');
-      menu_container.classList.remove('animate__fadeOutDown', 'animate__faster');
-
-      addAnimationLink();
-
-    } else {
-      
-      delay();
-      
-      img_container.classList.add('animate__fadeOutBottomLeft', 'animate__faster');
-      img_container.classList.remove('animate__fadeInBottomLeft', 'animate__fast');
-
-      icon_container.classList.remove('animate__fadeInRight', 'animate__fast');
-      icon_container.classList.add('animate__fadeOutRight','animate__faster');
-
-      text_container.classList.remove('animate__backInUp','animate__fast');
-      text_container.classList.add('animate__backOutDown', 'animate__faster');
-
-      menu_container.classList.remove('animate__fadeInUp', 'animate__fast');
-      menu_container.classList.add('animate__fadeOutDown', 'animate__faster');
-   }
-});
-
-// This function recognizes the ID of the element through the event parameter. Depending on the ID, the function
-// will add styles to the element si it can be moved by the mouse calculating the coordinates from clientX and clientY.
-
-const moveElement = (e) => {
+/* Recognizes the ID of the element through the event parameter. Depending on the ID, the function
+ will add styles to the element si it can be moved by the mouse calculating the coordinates from clientX and clientY. */
+ const moveElement = (e) => {
   let move = "";
   let x = "";
   let y = "";
@@ -64,15 +19,14 @@ const moveElement = (e) => {
     y = e.clientY * 100 / window.innerHeight - 50;
   } else if (e.target.id === "logo") {
     move = document.getElementById("logo");
-    x = e.clientX * 1500 / window.innerWidth - 50;
-    y = e.clientY * 1000 / window.innerHeight - 50;
+    x = e.clientX * 1000 / window.innerWidth - 50;
+    y = e.clientY * 800 / window.innerHeight - 50;
   }
   move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
   move.style.transition = "none";
 };
 
 // When the mouse leaves the element, it will restore it's position (translateY and translateX) to it's original location.
-
 const stopElement = (e) => {
   let stop = "";
   if (e.target.id === "title") {
@@ -86,7 +40,48 @@ const stopElement = (e) => {
   }, 300);
 };
 
-const delay = () => {
+// Remove animations when the nav menu opens / closes
+const removeAnimations = () => {
+  img_container.classList.add('animate__fadeOutBottomLeft', 'animate__faster');
+  img_container.classList.remove('animate__fadeInBottomLeft', 'animate__fast');
+
+  icon_container.classList.remove('animate__fadeInRight', 'animate__fast');
+  icon_container.classList.add('animate__fadeOutRight','animate__faster');
+
+  text_container.classList.remove('animate__backInUp','animate__fast');
+  text_container.classList.add('animate__backOutDown', 'animate__faster');
+
+  menu_container.classList.remove('animate__fadeInUp', 'animate__fast');
+  menu_container.classList.add('animate__fadeOutDown', 'animate__faster');
+
+  delayClose();
+}
+
+// Add animations when the nav menu opens / closes
+const addAnimations = () => {
+  section_1.classList.remove('d-block');
+  section_1.classList.add('d-none');
+  section_2.classList.remove('d-none');
+  section_2.classList.add('d-block');
+
+  img_container.classList.add('animate__fadeInBottomLeft', 'animate__fast');
+  img_container.classList.remove('animate__fadeOutBottomLeft', 'animate__faster');
+
+  icon_container.classList.add('animate__fadeInRight', 'animate__fast');
+  icon_container.classList.remove('animate__fadeOutRight', 'animate__faster');
+
+  text_container.classList.add('animate__backInUp', 'animate__fast');
+  text_container.classList.remove('animate__backOutDown', 'animate__faster');
+
+  menu_container.classList.add('animate__fadeInUp', 'animate__fast');
+  menu_container.classList.remove('animate__fadeOutDown', 'animate__faster');
+ 
+  // Add animations to nav menu links   
+  addAnimationsToMenu();
+}
+
+// Delays nav menu closing so animations effects might be seen
+const delayClose = () => {
   setTimeout(() => {
     section_1.classList.remove('d-none');
     section_1.classList.add('d-block');
@@ -95,7 +90,8 @@ const delay = () => {
   }, 300);
 }
 
-const addAnimationLink = () => {
+// Add animations to nav menu links (functions process)   
+const addAnimationsToMenu = () => {
   for (let i = 0; i < menu_link.length; i++) {
     const link = menu_link[i];
     i === 0 
@@ -103,6 +99,18 @@ const addAnimationLink = () => {
       : link.classList.add('animate__fadeInUp', 'animate__slower');
   }
 }
+
+// LISTENERS
+btn_label.addEventListener('click', () => {
+  boolean = !boolean;
+  //Add or remove animations when the nav menu opens / closes
+  boolean ? addAnimations() : removeAnimations();
+});
+
+title.addEventListener("mousemove", moveElement);
+title.addEventListener("mouseout", stopElement);
+logo.addEventListener("mousemove", moveElement);
+logo.addEventListener("mouseout", stopElement);
 
 
 
---
+++ js/variables.js
@@ -0,0 +1,16 @@
+// VARIABLES - ELEMENTS
+const section_1 = document.getElementById('section-1');
+const section_2 = document.getElementById('section-2');
+const btn_label = document.getElementById('btn-burger');
+const img_container = document.getElementById('img-container');
+const icon_container = document.getElementById('iconContainer');
+const text_container = document.getElementById('text-container');
+const menu_container = document.getElementById('menu-container');
+const menu_link = document.getElementsByClassName('menu-link');
+const logo = document.getElementById('logo');
+const title = document.getElementById('title');
+
+export { 
+  section_1, section_2, btn_label, img_container, icon_container, 
+  text_container, menu_container, menu_link, logo, title
+}
